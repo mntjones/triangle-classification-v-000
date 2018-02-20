3f36957ea425726ee5446c998a76218f016d8735
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-20 20:33:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,18 +9,17 @@ class Triangle
   
   def kind
     # illegal triangles
-    if @side1 =< 0 || @side2 =< 0 || @side3 =< 0
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       raise TriangleError
     elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       raise TriangleError
-    else #valid triangles
-      if @side1==@side2==@side3
-        :equilateral
-      elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
-        :isosceles
-      else
-        :scalene
-      end
+      #valid triangle
+    elsif @side1==@side2==@side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      :isosceles
+    else
+      :scalene
     end
     
   end
