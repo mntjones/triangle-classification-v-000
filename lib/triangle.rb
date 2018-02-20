31d97fb0d1929366eb4b93882bc075de49f35d0d
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-20 20:33:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       raise TriangleError
       #valid triangle
-    elsif @side1==@side2==@side3
+    elsif @side1==@side2 && @side1==@side3
       :equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
       :isosceles
